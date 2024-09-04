ed336e644804d5fa116bfa67032c85281256b413
oguhmr <oguhmr@gmail.com>
2024-09-04 12:25:36 -0600

    Ajuste 1

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/oguhmr/nodejsapp-master.git', 'develop') { node ->
+            node / gitConfigName('oguhmr')
+            node / gitConfigEmail('oguhmr@gmail.com')
         }
     }
     triggers {
